8414e3bc788f8ddfe14910db0b1625d8d6a2c281
Frederick Morrison <frederick.morrison@outlook.com>
2023-12-16 15:36:34 -0700

    add check for return statement in function

--- autograder_CODEHS.py
+++ autograder_CODEHS.py
@@ -3,6 +3,7 @@ class Suite(PythonTestSuite):
     inputs = ["Mark"]
 
     def expect_def(self) -> bool:
+        expectation = 'contains def'
         looking_for = 'def '
         retval = False
 
@@ -11,10 +12,11 @@ class Suite(PythonTestSuite):
             self.code_index = len(looking_for)
             retval = True
         expect(idx_def).to_be_greater_than_or_equal_to(0) \
-            .with_options(test_name="contains def")
+            .with_options(test_name=expectation)
         return retval
 
     def expect_function_name(self) -> bool:
+        expectation = 'contains function named hello (all lower case)'
         looking_for = 'hello'
         retval = False
         # verify the function is named hello (case sensitive)
@@ -24,7 +26,21 @@ class Suite(PythonTestSuite):
             retval = True
 
         expect(idx_fn_name).to_be_greater_than(0) \
-            .with_options(test_name='contains function named hello (all lower case)')
+            .with_options(test_name=expectation)
+
+        return retval
+
+    def expect_return_statement(self) -> bool:
+        expectation = 'hello function contains a return statement'
+        looking_for = 'return '
+        retval = False
+        idx_return = self.code_lower[self.code_index:].find(looking_for)
+        if idx_return >= 0:
+            self.code_index += idx_return + len(looking_for)
+            retval = True
+
+        expect(idx_return).to_be_greater_than_or_equal_to(0) \
+            .with_options(test_name=expectation)
 
         return retval
 
@@ -44,6 +60,7 @@ class Suite(PythonTestSuite):
             return retval
 
         # find the name parameter immediately following the left paren
+        expectation = 'hello function expects a name parameter'
         looking_for = 'name'
         idx_param = self.code_lower[self.code_index:].find(looking_for)
         if idx_param >= 0:
@@ -51,7 +68,7 @@ class Suite(PythonTestSuite):
             retval = True
 
         expect(idx_param).to_be_greater_than_or_equal_to(0) \
-            .with_options(test_name='hello function expects a name parameter')
+            .with_options(test_name=expectation)
 
         return retval
 
@@ -66,8 +83,10 @@ class Suite(PythonTestSuite):
         # start checking first line of code where def is supposed to be located
         if self.expect_def():
             if self.expect_function_name():
-                if self.expect_function_parameter():
-                    pass
+                self.expect_function_parameter()
+
+        # now look at the function return statement
+        self.expect_return_statement()
 
     # Write any tests that should run after the code is evaluated
     def after_run(self, student_code, solution_code, student_output, solution_output):
